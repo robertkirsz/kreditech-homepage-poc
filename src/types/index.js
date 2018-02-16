1928669b30d9e54395054a3edda566164cbaf0b3
Robert Kirsz <robert.kirsz@gmail.com>
2018-02-17 00:05:06 +0100

    Refactor types

--- src/types/index.js
+++ src/types/index.js
@@ -1,13 +1,14 @@
 import { shape, string, bool } from 'prop-types'
 
+import { queries } from 'styles'
+
 export const location = shape({
   pathname: string.isRequired
 }).isRequired
 
-// TODO: fetch values from styles.js
-export const media = shape({
-  smallOnly: bool.isRequired,
-  mediumUp: bool.isRequired,
-  mediumDown: bool.isRequired,
-  largeUp: bool.isRequired
-}).isRequired
+export const media = shape(
+  queries.reduce(
+    (all, query) => ({ ...all, [query.name]: bool.isRequired }),
+    {}
+  )
+).isRequired

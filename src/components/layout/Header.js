1bacf4dc50e423640ea432c31c0793314457fe43
Robert Kirsz <robert.kirsz@gmail.com>
2018-02-17 00:04:22 +0100

    Add header and intro section

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
 import { media } from 'types'
 import { Div, Container, Row, Col } from 'components'
 
+import kreditechLogo from 'assets/logos/kreditech.svg'
+import hamburgerIcon from 'assets/icons/hamburger.svg'
+
 export const Header = (props, { media }) => (
-  <Wrapper padding="24px 0">
-    <Container column>
+  <Wrapper padding="30px 0">
+    <Container>
       <Row>
         <Col small={3}>
-          Header
+          <NavLink to="/">
+            <img src={kreditechLogo} alt="" />
+          </NavLink>
         </Col>
-        <Col small={9} listRight={24}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
+        <Col small listRight={30} itemsCenter>
+          <StyledNavLink to="/people">People</StyledNavLink>
+          <StyledNavLink to="/solutions">Solutions</StyledNavLink>
+          <StyledNavLink to="/company">Company</StyledNavLink>
+          <StyledNavLink to="/carrers" style={{ marginLeft: 'auto' }}>Carrers</StyledNavLink>
+          <StyledNavLink to="/investors">Investors</StyledNavLink>
+          <StyledNavLink to="/contact">Contact us</StyledNavLink>
+          <img src={hamburgerIcon} alt="" />
         </Col>
       </Row>
     </Container>
@@ -22,4 +33,15 @@ export const Header = (props, { media }) => (
 
 Header.contextTypes = { media }
 
-const Wrapper = Div.withComponent('header')
+const Wrapper = Div.withComponent('header').extend`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+`
+
+const StyledNavLink = styled(NavLink)`
+  color: white;
+  font-weight: 600;
+  line-height: 24px;
+`
--- src/pages/home/IntroSection.js
+++ src/pages/home/IntroSection.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import { media } from 'types'
+
 import { Section, Container, LargeHeading } from 'components'
 
-export const IntroSection = () => (
-  <Section flex={1}>
+import background from 'assets/backgrounds/home-page-intro.jpg'
+
+const contextTypes = { media }
+
+export const IntroSection = (props, { media }) => {
+  console.log('media', media)
+  return (
+  <Section
+    backgroundImage={background}
+    cover
+    fullHeight={media.largeUp}
+  >
     <Container>
       <LargeHeading center>IntroSection</LargeHeading>
     </Container>
   </Section>
-)
+)}
+
+IntroSection.contextTypes = contextTypes

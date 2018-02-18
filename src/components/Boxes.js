07f62fd8112398efb4019f996c28ab60976d6ec9
Robert Kirsz <robert.kirsz@gmail.com>
2018-02-19 00:42:35 +0100

    Add green box

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -24,7 +24,10 @@ export class Boxes extends Component {
     return (
       <Wrapper {...this.props}>
         <BlueBox state={this.props.state} />
-        <YellowBox state={this.props.state} inverted={this.props.inverted} />
+        <BoxWrapper state={this.props.state} inverted={this.props.inverted}>
+          <YellowBox state={this.props.state} />
+          <GreenBox state={this.props.state} inverted={this.props.inverted} />
+        </BoxWrapper>
       </Wrapper>
     )
   }
@@ -42,8 +45,6 @@ const Box = styled.div`
   height: 644px;
   width: 350px;
   border-radius: 54px;
-  transform: skew(-12.5deg) translateX(-10px);
-  transform-origin: left bottom;
 
   ${props => props.state !== 'active' && 'opacity: 0;'}
 
@@ -55,13 +56,25 @@ const Box = styled.div`
 const BlueBox = Box.extend`
   top: 0;
   left: 0;
-
+  transform: skew(-12.5deg) translateX(-10px);
+  transform-origin: left bottom;
   background: ${colors.blue};
 `
 
-const YellowBox = Box.extend`
+const BoxWrapper = Box.extend`
   bottom: ${props => (props.inverted ? '-190' : '95')}px;
   left: ${props => (props.inverted ? '190' : '260')}px;
+  transform: skew(-12.5deg) translateX(-10px);
+  transform-origin: left bottom;
+  overflow: hidden;
+`
 
+const YellowBox = Box.extend`
   background: ${colors.yellow};
+`
+
+const GreenBox = Box.extend`
+  background: ${colors.green};
+  left: ${props => (props.inverted ? '-232' : '-240')}px;
+  top: ${props => (props.inverted ? '-191' : '94')}px;
 `
